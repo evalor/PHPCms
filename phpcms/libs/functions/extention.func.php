d859ce87e46c66d3c41892a1b63ccba9c122f056
evalor <master@evalor.cn>
2018-11-15 13:10:51 +0800

    fix cs /phpcms/extention.func.php

--- phpcms/libs/functions/extention.func.php
+++ phpcms/libs/functions/extention.func.php
@@ -2,8 +2,8 @@
 /**
  *  extention.func.php 用户自定义函数库
  *
- * @copyright			(C) 2005-2010 PHPCMS
- * @license				http://www.phpcms.cn/license/
- * @lastmodify			2010-10-27
+ * @copyright            (C) 2005-2010 PHPCMS
+ * @license                http://www.phpcms.cn/license/
+ * @lastmodify            2010-10-27
  */
  
